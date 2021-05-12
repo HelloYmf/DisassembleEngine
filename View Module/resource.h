5d3e7d9affbb2f695a1c327724f240652db6d6c7
HelloYmf <60568618+HelloYmf@users.noreply.github.com>
2021-05-12 18:47:04 +0800

    Add files via upload

---
+++ View Module/resource.h
@@ -0,0 +1,22 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 反汇编引擎1.0.rc 使用
+//
+#define IDD_DIALOG_MAIN                 101
+#define IDC_LIST1                       1001
+#define IDC_LIST_MAIN                   1001
+#define IDC_EDIT1                       1002
+#define IDC_EDIT_DESFILE                1002
+#define IDC_BUTTON_CHOOSEFILE           1004
+#define IDC_BUTTON_DASM                 1005
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
